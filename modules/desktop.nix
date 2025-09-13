c87096ab6d746d0652f888ed1c25385aeca6ae15
sekhudin <sekhudinuap@gmail.com>
2025-09-13 17:18:24 +0700

    desktop(vscode): add vscode shortcut

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,5 +3,8 @@
 {
   home.file = {
     ".local/share/applications/kitty.desktop".source = ../.desktop/kitty.desktop;
+    ".local/share/applications/vscode.desktop".source = ../.desktop/vscode.desktop;
+    ".local/share/applications/vscode-work.desktop".source = ../.desktop/vscode-work.desktop;
+    ".local/share/applications/vscode-me.desktop".source = ../.desktop/vscode-me.desktop;
   };
 }
--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,8 +1,34 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
+  ];
+
   programs.vscode = {
      enable = true;
-     mutableExtensionsDir = true;
+     mutableExtensionsDir = false;
+     profiles.default = {
+       extensions = [
+         pkgs.vscode-extensions.esbenp.prettier-vscode
+       ];
+     };
+
+     profiles.me = {
+       extensions = [
+         pkgs.vscode-extensions.esbenp.prettier-vscode
+       ];
+     };
+
+     profiles.work = {
+       extensions = [
+       ];
+     };
+  };
+
+  home.shellAliases = {
+    code = "code --no-sandbox";
+    "code:me" = "code --profile me --no-sandbox";
+    "code:work" = "code --profile work --no-sandbox";
   };
 }
